02c65dab1068a72ffabd588fdfb700d6e06b0c4f
Riccardo Torregiani <torrki@riccardo-torregiani-Inspiron-15-3520.broadband>
2026-05-01 15:30:02 +0200

    Inserito automa di rilevazione di errore di sintassi
    Signed-off-by: Riccardo Torregiani <torrki@riccardo-torregiani-Inspiron-15-3520.broadband>

--- src/AlberoSintassi.c
+++ src/AlberoSintassi.c
@@ -8,16 +8,20 @@ int prioritaOperatori(char uo, char o);
 const char* ordineOperatori="./*+-";
 
 void StampaAlbero(Nodo n, unsigned livello){
-	for(unsigned i=0; i < livello; i++){
-		printf(i == livello-1 ? "|" : "-");
-	}
-	
-	if(n.no->tipo=='o'){
-		printf("%c\n", n.no->op);
-		StampaAlbero(n.no->n1,livello+1);
-		StampaAlbero(n.no->n2,livello+1);
+	if(n.nf != (struct NodoFunzione*)NULL){
+		for(unsigned i=0; i < livello; i++){
+			printf(i == livello-1 ? "|" : "-");
+		}
+		
+		if(n.no->tipo=='o'){
+			printf("%c\n", n.no->op);
+			StampaAlbero(n.no->n1,livello+1);
+			StampaAlbero(n.no->n2,livello+1);
+		}else{
+			printf("%s\n", n.nf->f);
+		}
 	}else{
-		printf("%s\n", n.nf->f);
+		printf("Indirizzo dell'albero da stampare NULL\n");
 	}
 }
 
@@ -30,17 +34,19 @@ Nodo CreaAlbero(const char* funzioneSym, size_t* numeroCaratteri){
 	nodoCorrente.no=NULL;
 	nodoRadice.no=NULL;
 	sottoAlberoParentesi.no=NULL;
+	char statoSintassi='f';	//variabile di stato per alternare la lettura tra funzioni e operatori
 	
 	char* bufferStringa=(char*)calloc(100,sizeof(char));
 	char ultimoOperatore='.'; //operatore a massima priorità
+	char condizioneCiclo=1; //la condizione del ciclo è se non ci sono stati errori di sintassi o fine della stringa
 	unsigned i=0;
-	
+		
 	//processamento della stringa
-	while(*funzioneSym && *funzioneSym != ')'){
+	while(condizioneCiclo){
 		char carattere=*funzioneSym;
 		
 		//se il carattere letto è una funzione o un operatore
-		if(operatore(carattere)){
+		if(statoSintassi == 'o' && operatore(carattere)){
 			bufferStringa[i]='\0';
 			NodoOperatore* nuovoOp=(NodoOperatore*)malloc(sizeof(NodoOperatore));			
 			Nodo funzioneOp;
@@ -81,44 +87,52 @@ Nodo CreaAlbero(const char* funzioneSym, size_t* numeroCaratteri){
 			}
 			
 			//torno a inizio buffer
+			statoSintassi='f';
 			i=0;
-		}else if(carattere == '('){
+		}else if(statoSintassi == 'f' && carattere == '('){
 			//Il contenuto tra parentesi ha maggiore priorità e bisogna creare un albero specifico
-			sottoAlberoParentesi=CreaAlbero(funzioneSym+1,&caratteriSottoalbero);			
-			//StampaAlbero(sottoAlberoParentesi,0);
-		}else{
+			sottoAlberoParentesi=CreaAlbero(funzioneSym+1,&caratteriSottoalbero);
+			statoSintassi='o';
+		}else if(statoSintassi == 'o' && (*funzioneSym=='\n' || *funzioneSym==')') ){ //fine del ciclo
+			//lettura ultima funzione
+			Nodo ultimaFunzione;
+			if(sottoAlberoParentesi.no == NULL){ //se non ci sono state parentesi a separare l'operatore
+				bufferStringa[i]='\0';
+				ultimaFunzione.nf=(NodoFunzione*)malloc(sizeof(NodoFunzione));
+				ultimaFunzione.nf->tipo='f';
+				ultimaFunzione.nf->f=(char*)malloc(strlen(bufferStringa)+1);
+				strcpy(ultimaFunzione.nf->f, (const char *)bufferStringa);
+			}else{
+				ultimaFunzione=sottoAlberoParentesi;
+				sottoAlberoParentesi.no=NULL;
+			}
+			
+			if(livello_max==0){	//se non ho mai incontrato un operatore
+				nodoRadice=ultimaFunzione;
+			}else{	//completo l'ultimo operatore
+				nodoCorrente.no->n2=ultimaFunzione;
+			}
+			
+			*numeroCaratteri=caratteriProcessati;
+			condizioneCiclo=0;
+		}else if(statoSintassi == 'f' && !operatore(carattere)){
 			bufferStringa[i]=carattere;
 			i++;
+			statoSintassi='o';			
+		}else{
+			printf("Errore nella sintassi della funzione al carattere %ld\n", caratteriProcessati+1);
+			condizioneCiclo=0;
+			nodoRadice.nf=(struct NodoFunzione*)NULL;
 		}
 		funzioneSym += sottoAlberoParentesi.nf == NULL ? 1 : caratteriSottoalbero+2;
 		caratteriProcessati += sottoAlberoParentesi.nf == NULL ? 1 : caratteriSottoalbero+2;
 	}
 	
-	//lettura ultima funzione
-	Nodo ultimaFunzione;
-	if(sottoAlberoParentesi.no == NULL){ //se non ci sono state parentesi a separare l'operatore
-		bufferStringa[i]='\0';
-		ultimaFunzione.nf=(NodoFunzione*)malloc(sizeof(NodoFunzione));
-		ultimaFunzione.nf->tipo='f';
-		ultimaFunzione.nf->f=(char*)malloc(strlen(bufferStringa)+1);
-		strcpy(ultimaFunzione.nf->f, (const char *)bufferStringa);
-	}else{
-		ultimaFunzione=sottoAlberoParentesi;
-		sottoAlberoParentesi.no=NULL;
-	}
-	
-	if(livello_max==0){	//se non ho mai incontrato un operatore
-		nodoRadice=ultimaFunzione;
-	}else{	//completo l'ultimo operatore
-		nodoCorrente.no->n2=ultimaFunzione;
-	}
-	
-	*numeroCaratteri=caratteriProcessati;
 	return nodoRadice;
 }
 
 int operatore(char c){
-	return (c=='+') | (c=='-') | (c=='*') | (c=='/');
+	return (c==ordineOperatori[1]) | (c==ordineOperatori[2]) | (c==ordineOperatori[3]) | (c==ordineOperatori[4]);
 }
 
 int prioritaOperatori(char uo, char o){
