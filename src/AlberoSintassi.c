6f3403e5b1635035a2d7875dee3d95879e2efc9f
Torrki <kioer.exe@gmail.com>
2026-05-12 23:22:08 +0200

    Inserita la semplificazione delle espressioni con numeri
    Signed-off-by: Torrki <kioer.exe@gmail.com>

--- src/AlberoSintassi.c
+++ src/AlberoSintassi.c
@@ -6,6 +6,14 @@ int operatore(char c);
 int prioritaOperatori(char uo, char o);
 const char* ordineOperatori=".@^/*+-";
 
+char SoloNumeri(const char* stringa);
+
+char SoloNumeri(const char* stringa){
+	size_t conteggio=0;
+	while('0' <= stringa[conteggio] && stringa[conteggio] <= '9') conteggio++;
+	return conteggio == strlen(stringa);
+}
+
 void StampaAlbero(Nodo n, unsigned livello){
 	if(n.ni != (struct NodoInfo*)NULL){
 		for(unsigned i=0; i < livello; i++){
@@ -98,7 +106,7 @@ Nodo CreaAlbero(struct AutomaSintassi *a, const char* funzioneSym, size_t* numer
 				}else{	//passi successivi
 					nodoCorrente.no->n2=funzioneOp;
 					funzioneOp.ni->genitore=nodoCorrente.no;
-					
+										
 					//cerco l'ultimo operatore per mantenere le priorità
 					NodoOperatore* genitore=nodoCorrente.ni->genitore;
 					while(genitore && prioritaOperatori(genitore->op,op)){
@@ -108,6 +116,7 @@ Nodo CreaAlbero(struct AutomaSintassi *a, const char* funzioneSym, size_t* numer
 						nuovoOp->n1=genitore->n2;
 						genitore->n2.no->genitore=nuovoOp;
 						genitore->n2.no=nuovoOp;
+						
 						nuovoOp->genitore=genitore;
 					}else{
 						nuovoOp->n1=nodoRadice;
@@ -193,33 +202,53 @@ const char* AnalizzaAlbero(struct Tabella* tabella, Nodo radice){
 			//risolvo gli operandi
 			const char* derivataOpSx=AnalizzaAlbero(tabella,radice.no->n1);
 			const char* derivataOpDx=AnalizzaAlbero(tabella,radice.no->n2);
+			char numeroSx=strcmp(derivataOpSx,"0")==0, numeroDx=strcmp(derivataOpDx,"0")==0; 
 			size_t dimSx=strlen(derivataOpSx);
 			size_t dimDx=strlen(derivataOpDx);
 			const char *OpDx, *OpSx;
 			
 			switch(radice.no->op){
 				case '+':
-					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+2));
-					sprintf(stringaDerivata,"%s+%s",derivataOpSx,derivataOpDx);
+					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+6));
+					
+					if(numeroSx || numeroDx){
+						sprintf(stringaDerivata,"%s",numeroSx ? derivataOpDx : derivataOpSx);
+					}else{
+						sprintf(stringaDerivata,"(%s)+(%s)",derivataOpSx,derivataOpDx);
+					}
 					break;
 				case '-':
-					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+2));
-					sprintf(stringaDerivata,"%s-%s",derivataOpSx,derivataOpDx);
+					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+6));
+					
+					if(numeroSx || numeroDx){
+						sprintf(stringaDerivata,"%s",numeroSx ? derivataOpDx : derivataOpSx);
+					}else{
+						sprintf(stringaDerivata,"(%s)-(%s)",derivataOpSx,derivataOpDx);
+					}
 					break;
 				case '*':
 					OpSx=AlberotoStr(radice.no->n1);
 					OpDx=AlberotoStr(radice.no->n2);
-					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+strlen(OpSx)+strlen(OpDx)+16));
-					sprintf(stringaDerivata,"((%s)*(%s))+((%s)*(%s))",derivataOpSx,OpDx,OpSx,derivataOpDx);
+					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+strlen(OpSx)+strlen(OpDx)+12));
 					
+					if(numeroSx || numeroDx){
+						sprintf(stringaDerivata,"%s*(%s)", numeroSx ? OpSx : OpDx, numeroSx ? derivataOpDx : derivataOpSx);
+					}else{
+						sprintf(stringaDerivata,"(%s)*(%s)+(%s)*(%s)",derivataOpSx,OpDx,OpSx,derivataOpDx);
+					}
 					free((void*)OpSx);
 					free((void*)OpDx);
 					break;
 				case '/':
 					OpSx=AlberotoStr(radice.no->n1);
 					OpDx=AlberotoStr(radice.no->n2);
-					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+strlen(OpSx)+strlen(OpDx)*2+24));
-					sprintf(stringaDerivata,"(((%s)*(%s))-((%s)*(%s)))/((%s)^2)",derivataOpSx,OpDx,OpSx,derivataOpDx,OpDx);
+					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+strlen(OpSx)+strlen(OpDx)*2+21));
+					
+					if(numeroSx || numeroDx){
+						sprintf(stringaDerivata,"(%s%s*(%s))/((%s)^2)",numeroSx ? "-" : "", numeroSx ? OpSx : OpDx, numeroSx ? derivataOpDx : derivataOpSx, OpDx);
+					}else{
+						sprintf(stringaDerivata,"((%s)*(%s)-(%s)*(%s))/((%s)^2)",derivataOpSx,OpDx,OpSx,derivataOpDx,OpDx);
+					}
 					
 					free((void*)OpSx);
 					free((void*)OpDx);
@@ -231,7 +260,12 @@ const char* AnalizzaAlbero(struct Tabella* tabella, Nodo radice){
 					
 					stringaDerivata=(char*)malloc(sizeof(char)*(strlen(DerivataComposta)+dimDx+6));
 					
-					sprintf(stringaDerivata,"(%s)*(%s)",DerivataComposta,derivataOpDx);
+					if(numeroDx){
+						strcpy(stringaDerivata,"0");
+					}else{
+						sprintf(stringaDerivata,"(%s)*(%s)",DerivataComposta,derivataOpDx);
+					}
+					
 					free((void*)OpDx);
 					free((void*)DerivataComposta);
 					break;
@@ -239,7 +273,14 @@ const char* AnalizzaAlbero(struct Tabella* tabella, Nodo radice){
 					OpSx=AlberotoStr(radice.no->n1);
 					OpDx=AlberotoStr(radice.no->n2);
 					stringaDerivata=(char*)malloc(sizeof(char)*(dimSx+dimDx+3*strlen(OpSx)+2*strlen(OpDx)+29));
-					sprintf(stringaDerivata,"((%s)^(%s))*((%s)*ln(%s)+(%s)*((%s)/(%s)))",OpSx,OpDx,derivataOpDx,OpSx,OpDx,derivataOpSx,OpSx);
+					
+					if(numeroSx && !numeroDx){
+						sprintf(stringaDerivata,"(%s^(%s))*(%s)*ln(%s)",OpSx,OpDx,derivataOpDx,OpSx);
+					}else if(numeroDx && !numeroSx){
+						sprintf(stringaDerivata,"((%s)^%s)*%s*((%s)/(%s))",OpSx,OpDx,OpDx,derivataOpSx,OpSx);
+					}else{
+						sprintf(stringaDerivata,"((%s)^(%s))*((%s)*ln(%s)+(%s)*((%s)/(%s)))",OpSx,OpDx,derivataOpDx,OpSx,OpDx,derivataOpSx,OpSx);
+					}
 					
 					free((void*)OpSx);
 					free((void*)OpDx);
